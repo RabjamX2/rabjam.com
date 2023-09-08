dc54f5b0975dc66545ec399db5d61702eb0d7f1e
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2023-09-08 11:14:35 -0400

    created directory

--- assets/js/pi.js
+++ assets/js/pi.js
@@ -1,29 +1,3 @@
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const body = document.body;
-
-// Check if the user has a preferred color scheme
-const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-// Function to set the theme
-function setTheme(isDarkMode) {
-    if (isDarkMode) {
-        body.classList.add("dark-mode");
-        darkModeToggle.classList.add("dark-mode");
-    } else {
-        body.classList.remove("dark-mode");
-        darkModeToggle.classList.remove("dark-mode");
-    }
-}
-
-// Toggle dark mode when the button is clicked
-darkModeToggle.addEventListener("click", () => {
-    const isDarkMode = body.classList.contains("dark-mode");
-    setTheme(!isDarkMode);
-});
-
-// Initialize the theme based on user preference
-setTheme(prefersDarkMode);
-
 // JavaScript for your dynamic content
 const canvasContainer = document.getElementById("canvasContainer");
 const canvas = document.getElementById("piRandom");
